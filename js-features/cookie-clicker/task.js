71c88d6baea611945caeaf0faa335f80b8e9d7b7
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-03-13 19:08:22 +0500

    Вторая задача

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,32 @@
+const cookie = document.getElementById('cookie');
+const clickerCounter = document.getElementById('clicker__counter');
+const clickerSpeed = document.getElementById('clicker__speed');
+const date = Date.now();
+let lastClick = 0;
+
+console.log(cookie);
+console.log(clickerCounter);
+console.log(clickerSpeed);
+console.log(date);
+console.log(date + 'Текущее')
+
+function resize() {
+    clickerCounter.textContent ++;
+    speed();
+    if(cookie.width === 100) {
+        cookie.width = 200;
+    } else {
+        cookie.width = 100;
+    }
+}
+
+
+
+function speed() {
+    const clickNow = Date.now();
+    const formula = (1 / ((clickNow - lastClick) / 1000)).toFixed(2);
+    clickerSpeed.textContent = formula;
+    lastClick = clickNow;
+}
+
+cookie.onclick = resize;
