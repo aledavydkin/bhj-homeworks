bc4f7077c4e2bdfc91a5781378e0a7aa17783ac6
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-03-26 19:19:49 +0500

    2. Задача исправлена

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,17 +1,23 @@
 const menuLink = Array.from(document.getElementsByClassName('menu__link'));
 
-for(let i = 0; i < menuLink.length; i++) {
-    menuLink[i].onclick = function () {
-        let parent = menuLink[i].parentElement.querySelector('ul');
-        let active = menuLink[i].closest('.menu_main').querySelector('.menu_active');
 
-        if(active) {
+menuLink.forEach(el => {
+    el.onclick = function () {
+        let parent = el.parentElement.querySelector('ul');
+        let active = el.closest('.menu_main').querySelector('.menu_active');
+
+        if (parent.classList.contains('menu_active')) {
             active.classList.remove('menu_active');
-            parent.classList.add('menu_active')
         } else {
-            parent.classList.add('menu_active')
+            if(active) {
+                active.classList.remove('menu_active');
+                parent.classList.add('menu_active')
+            } else {
+                parent.classList.add('menu_active')
+            }
         }
         return false;
     };
-}
+})
+
 
