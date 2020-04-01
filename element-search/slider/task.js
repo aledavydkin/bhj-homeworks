8799d8a615f366914e543a954665920b7531832c
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-04-01 11:12:46 +0500

    3 Задание

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,43 +1,46 @@
-let sliderElements = Array.from(document.querySelectorAll('.slider__item'));
-let dots = Array.from(document.querySelectorAll('.slider__dot'));
-
-let arrow = Array.from(document.querySelectorAll('.slider__arrow'));
+let sliderElements = document.querySelectorAll(".slider__item");
+const dots = document.querySelectorAll(".slider__dot");
+const prev = document.querySelector(".slider__arrow_prev");
+const next = document.querySelector(".slider__arrow_next");
 
 let index = 0;
 
-for(let i = 0; i < arrow.length; i++) {
-    arrow[i].onclick = changeSlides;
+function changeActive(index) {
+    if (sliderElements.item(index).classList.contains("slider__item_active")) {
+        sliderElements.item(index).classList.remove("slider__item_active");
+        dots.item(index).classList.remove("slider__dot_active");
+    } else {
+        sliderElements.item(index).classList.add("slider__item_active");
+        dots.item(index).classList.add("slider__dot_active");
+    }
 }
 
-for(let i = 0; i < dots.length; i++) {
-    dots[i].onclick = dotsChangeSlides(i);
-}
+next.onclick = function() {
+    changeActive(index);
+    index++;
 
-function changeSlides(e) {
-    sliderElements[index].classList.remove('slider__item_active');
-
-    let arrowNext = e.target.classList.contains('slider__arrow_next');
-    let arrowPrev = e.target.classList.contains('slider__arrow_prev');
-
-    if(arrowNext) {
-        if (index === sliderElements.length - 1) {
-            index = 0;
-        } else {
-            index++;
-        }
+    if (index >= sliderElements.length) {
+        index = 0;
     }
 
-    if(arrowPrev) {
-        if (index === 0) {
-            index = sliderElements.length - 1;
-        } else {
-            index--;
-        }
+    changeActive(index);
+};
+
+prev.onclick = function() {
+    changeActive(index);
+    index--;
+
+    if (index < 0) {
+        index = sliderElements.length - 1;
     }
 
-    sliderElements[index].classList.add('slider__item_active');
-}
+    changeActive(index);
+};
 
-function dotsChangeSlides(index) {
-    dots[index].classList.add('slider__dot_active');
+for (let i = 0; i < dots.length; i++) {
+    dots.item(i).onclick = function() {
+        changeActive(index);
+        changeActive(i);
+        index = i;
+    }
 }
