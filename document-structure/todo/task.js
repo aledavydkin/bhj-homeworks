24399a376911ebaf1e56e4f592af72509757dfcd
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-04-02 11:24:02 +0500

    2 Задание

---
+++ document-structure/todo/task.js
@@ -0,0 +1,38 @@
+const taskInput = document.getElementById('task__input');
+const taskAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+const addItem = function(e) {
+    e.preventDefault();
+    let text = taskInput.value;
+
+    if(taskInput.value) {
+        tasksList.innerHTML += `
+        <div class="task">
+          <div class="task__title">
+            ${text}
+          </div>
+          <a href="#" class="task__remove">&times;</a>
+        </div>
+    `;
+        taskInput.value = '';
+
+        const taskRemove = Array.from(document.querySelectorAll('.task__remove'));
+        taskRemove.forEach(el => {
+            el.addEventListener('click', removeItem);
+        });
+    }
+};
+
+taskAdd.addEventListener('click', addItem);
+taskInput.addEventListener('keydown', function(e) {
+    if (e.keyCode === 13) {
+        addItem();
+    }
+});
+
+const removeItem = function(e) {
+   const taskRemove = e.target.closest('.task');
+   taskRemove.remove();
+};
+
