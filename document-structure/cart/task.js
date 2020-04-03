35198a477872e512848c1fd2e7785b72eebd231c
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-04-03 20:43:30 +0500

    3 задание исправление

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,7 +1,6 @@
 const productQuantityControls = Array.from(document.querySelectorAll('.product__quantity-controls'));
 const productAdd = Array.from(document.querySelectorAll('.product__add'));
 const cart__products = document.querySelector('.cart__products');
-let id = [];
 
 productQuantityControls.forEach(el => {
     const productQuantityControl = Array.from(el.querySelectorAll('.product__quantity-control'));
@@ -9,7 +8,7 @@ productQuantityControls.forEach(el => {
 
     const control = function (event) {
         if(event.target.classList.contains('product__quantity-control_dec')) {
-            if(Number(productQuantityValue.textContent) === 0) {
+            if(Number(productQuantityValue.textContent) === 1) {
                 return null;
             } else {
                 productQuantityValue.textContent --;
@@ -29,8 +28,9 @@ productAdd.forEach(el => {
         const product = event.target.closest('.product');
         const productQuantity = product.querySelector('.product__quantity-value').textContent;
         const image = product.querySelector('.product__image').getAttribute('src');
+        let productExist = Array.from(cart__products.children).find(el => el.dataset.id === product.dataset.id);
 
-        if(id.includes(product.dataset.id)) {
+        if(productExist) {
             const list = Array.from(cart__products.querySelectorAll('.cart__product'));
             list.forEach(el => {
                 if(product.dataset.id === el.dataset.id) {
@@ -42,13 +42,20 @@ productAdd.forEach(el => {
             })
         }
 
-        if(Number(productQuantity) !== 0 && !id.includes(product.dataset.id)) {
-            id.push(product.dataset.id);
-            cart__products.innerHTML += `
+        if(!productExist) {
+            cart__products.insertAdjacentHTML('afterbegin', `
                 <div class="cart__product" data-id="${product.dataset.id}">
                     <img class="cart__product-image" src="${image}">
                     <div class="cart__product-count">${productQuantity}</div>
-                </div>`
+                    <div class="cart__product-crest">Х</div>
+                </div>`)
+
+            let crossClick = cart__products.querySelector('.cart__product-crest');
+            crossClick.addEventListener('click', function(e) {
+                const crossRemove = e.target.closest('.cart__product');
+                crossRemove.remove();
+            });
+
         } else {
             return null;
         }
