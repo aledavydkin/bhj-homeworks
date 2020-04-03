ad4430ccb77af9975183c4a48f6e0053e1ea96d0
Alexandr Davyidkin <alexandr.davyidkin@eldorado.ru>
2020-04-03 17:51:49 +0500

    1 задание исправление

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -4,17 +4,21 @@ const tooltip = document.createElement('div');
 tooltips.forEach(el => {
     el.addEventListener('click', function (event) {
         event.preventDefault();
-        tooltip.innerText = '';
         const thisTarget = event.target;
-        const title = thisTarget.getAttribute('title');
-        let coord = el.getBoundingClientRect();
-        let left = el.dataset.position === 'right' ? coord.left + (coord.right - coord.left) +'px': coord.left+'px';
-        let top = el.dataset.position === 'top' ? coord.top + (coord.top - coord.bottom) +'px' : coord.bottom+'px';
+        const tooltipActive = thisTarget.querySelector('.tooltip');
 
-        tooltip.classList.add('tooltip', 'tooltip_active');
-        tooltip.insertAdjacentText("afterBegin",`${title}`);
-        tooltip.setAttribute('style',`left: ${left}; top: ${top}`);
-
-        thisTarget.insertAdjacentElement("afterBegin", tooltip);
+        if (!tooltipActive) {
+            tooltip.innerText = '';
+            const title = thisTarget.getAttribute('title');
+            let coord = el.getBoundingClientRect();
+            let left = el.dataset.position === 'right' ? coord.left + (coord.right - coord.left) +'px': coord.left+'px';
+            let top = el.dataset.position === 'top' ? coord.top + (coord.top - coord.bottom) +'px' : coord.bottom+'px';
+            tooltip.classList.add('tooltip', 'tooltip_active');
+            tooltip.insertAdjacentText("afterBegin",`${title}`);
+            tooltip.setAttribute('style',`left: ${left}; top: ${top}`);
+            thisTarget.insertAdjacentElement("afterBegin", tooltip);
+        } else {
+            tooltipActive.remove();
+        }
     })
 });
